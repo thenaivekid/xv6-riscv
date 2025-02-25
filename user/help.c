d0f9f2adeb3dab0379fa00c22c52c67b7b788ec9
thenaivekid <neupane.ashok.9696@gmail.com>
2025-02-25 20:21:11 +0545

    added Ashok$ and help cmd

---
+++ user/help.c
@@ -0,0 +1,10 @@
+////
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main() {
+    // Display the custom message
+    printf("This is our version of xv6. Made by ashok, anil and jeevan\n");
+    exit(0); // Exit the program
+}
--- user/sh.c
+++ user/sh.c
@@ -134,7 +134,8 @@ runcmd(struct cmd *cmd)
 int
 getcmd(char *buf, int nbuf)
 {
-  write(2, "$ ", 2);
+  // write(2, "$ ", 2);
+  write(2, "Ashok$ ", 7);
   memset(buf, 0, nbuf);
   gets(buf, nbuf);
   if(buf[0] == 0) // EOF
